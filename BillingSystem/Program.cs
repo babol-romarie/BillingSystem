2d492eef28b9264b8dcc154e75b70c3bc343d7b5
Mae Cumpleto <mcumpleto@gmail.com>
2026-08-30 22:58:12 +0800

    Member to update the program.cs

--- BillingSystem/Program.cs
+++ BillingSystem/Program.cs
@@ -12,9 +12,9 @@ namespace BillingSystem
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new LoginForm());
-            Application.Run(new LoginForm());
             Application.Run(new CustomerListForm());
             Application.Run(new AddCustomerForm());
+            // Application.Run(new EditCustomerForm());
         }
     }
 }
